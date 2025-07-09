948fa8650344b83f63368e5ef6ffc3d9b201c733
Mouse <mousefad@googlemail.com>
2025-07-09 22:23:34 +0100

    change defauilt log level

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,7 @@ struct Args {
 
 fn main() {
     let env = Env::default()
-        .filter_or("MY_LOG_LEVEL", "trace")
+        .filter_or("MY_LOG_LEVEL", "info")
         .write_style_or("MY_LOG_STYLE", "always");
     env_logger::init_from_env(env);
 
